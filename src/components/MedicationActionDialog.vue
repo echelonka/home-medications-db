06264fcc7d2d5255ce74bf18dcaa4430895fdda8
marinaskripnik97@gmail.com <marinaskripnik97@gmail.com>
2019-10-27 21:24:39 +0200

    edit medication functionality; description field

--- src/components/MedicationActionDialog.vue
+++ src/components/MedicationActionDialog.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-dialog :value="value" @input="hideAddingDialog" max-width="500px">
+  <v-dialog :value="value" @input="hideDialog" max-width="500px">
     <v-card>
       <v-card-title>
-        <span class="headline">Add Medication</span>
+        <span class="headline">{{ action | capitalize }} Medication</span>
       </v-card-title>
       <v-card-text>
         <v-row>
           <v-col cols="12" xs="12">
-            <v-text-field v-model="newMedication.name" label="Name" :error-messages="nameErrors"></v-text-field>
+            <v-text-field v-model="medication.name" label="Name" :error-messages="nameErrors"></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
             <v-select
               :items="categories"
               label="Category"
-              v-model="newMedication.category"
+              v-model="medication.category"
               :error-messages="categoryErrors"
             ></v-select>
           </v-col>
@@ -29,12 +29,12 @@
             >
               <template v-slot:activator="{ on }">
                 <v-text-field
-                  v-model="newMedication.expiration_date"
+                  v-model="medication.expiration_date"
                   label="Expiration date"
-                  hint="MM/DD/YYYY"
+                  hint="DD/MM/YYYY"
                   persistent-hint
                   prepend-icon="event"
-                  @blur="date = parseDate(newMedication.expiration_date)"
+                  @blur="date = parseDate(medication.expiration_date)"
                   v-on="on"
                   :error-messages="dateErrors"
                 ></v-text-field>
@@ -42,12 +42,15 @@
               <v-date-picker v-model="date" no-title @input="dateMenu = false"></v-date-picker>
             </v-menu>
           </v-col>
+          <v-col cols="12" xs="12">
+            <v-textarea label="Description (optional)" v-model="medication.description"></v-textarea>
+          </v-col>
         </v-row>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="hideAddingDialog">Cancel</v-btn>
-        <v-btn color="blue darken-1" text @click="addMedication" :loading="isSending">Add</v-btn>
+        <v-btn color="blue darken-1" text @click="hideDialog">Cancel</v-btn>
+        <v-btn color="blue darken-1" text @click="changeMedication" :loading="isSending">{{ action }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -60,6 +63,14 @@
   export default {
     name: 'AddMedicationDialog',
     props: {
+      action: {
+        type: String,
+        required: true,
+        validator: value => ['add', 'edit'].indexOf(value) !== -1
+      },
+      initialMedication: {
+        type: Object
+      },
       value: {
         type: Boolean,
         required: true
@@ -70,14 +81,15 @@
       date: null,
       dateMenu: false,
       isSending: false,
-      newMedication: {
+      medication: {
         name: null,
         category: null,
+        description: null,
         expiration_date: null
       }
     }),
     validations: {
-      newMedication: {
+      medication: {
         name: { required },
         category: { required },
         expiration_date: { required }
@@ -85,8 +97,17 @@
     },
     watch: {
       date (newDate) {
-        this.newMedication.expiration_date = this.formatDate(newDate)
+        this.medication.expiration_date = this.formatDate(newDate)
       },
+      value(value) {
+        if (this.action === 'edit' && value) {
+          this.medication = {
+            ...this.initialMedication,
+            category: this.initialMedication.category.id,
+            initialCategory: this.initialMedication.category.id
+          }
+        }
+      }
     },
     computed: {
       categories () {
@@ -97,20 +118,20 @@
       },
       categoryErrors () {
         const errors = []
-        if (!this.$v.newMedication.category.$dirty) return errors
-        !this.$v.newMedication.category.required && errors.push('Category is required.')
+        if (!this.$v.medication.category.$dirty) return errors
+        !this.$v.medication.category.required && errors.push('Category is required.')
         return errors
       },
       nameErrors () {
         const errors = []
-        if (!this.$v.newMedication.name.$dirty) return errors
-        !this.$v.newMedication.name.required && errors.push('Name is required.')
+        if (!this.$v.medication.name.$dirty) return errors
+        !this.$v.medication.name.required && errors.push('Name is required.')
         return errors
       },
       dateErrors () {
         const errors = []
-        if (!this.$v.newMedication.expiration_date.$dirty) return errors
-        !this.$v.newMedication.expiration_date.required && errors.push('Expiration date is required.')
+        if (!this.$v.medication.expiration_date.$dirty) return errors
+        !this.$v.medication.expiration_date.required && errors.push('Expiration date is required.')
         return errors
       }
     },
@@ -118,33 +139,34 @@
       formatDate (date) {
         if (!date) return null
         const [year, month, day] = date.split('-')
-        return `${ month }/${ day }/${ year }`
+        return `${ day }/${ month }/${ year }`
       },
       parseDate (date) {
         if (!date) return null
-        const [month, day, year] = date.split('/')
+        const [day, month, year] = date.split('/')
         return `${ year }-${ month.padStart(2, '0') }-${ day.padStart(2, '0') }`
       },
-      clearNewMedication () {
-        this.newMedication = {
+      clearMedication () {
+        this.medication = {
           name: null,
           category: null,
+          description: null,
           expiration_date: null
         }
         this.$v.$reset()
       },
-      hideAddingDialog () {
-        this.clearNewMedication()
+      hideDialog () {
+        this.clearMedication()
         this.$emit('input', false)
       },
-      async addMedication () {
+      async changeMedication () {
         this.$v.$reset()
         this.$v.$touch()
         if (this.$v.$invalid) return false
 
         this.isSending = true
-        await this.$store.dispatch('addMedication', this.newMedication)
-        this.clearNewMedication()
+        await this.$store.dispatch(`${ this.action }Medication`, this.medication)
+        this.clearMedication()
         this.isSending = false
         this.$emit('input', false)
       }
--- src/pages/Dashboard/Medications.vue
+++ src/pages/Dashboard/Medications.vue
@@ -1,59 +1,74 @@
 <template>
   <v-card>
-    <v-data-table :headers="headers" :items="allMedications()" :search="searchString" disable-pagination hide-default-footer>
+    <v-data-table
+      :headers="headers"
+      :items="allMedications()"
+      :search="searchString"
+      disable-pagination
+      hide-default-footer
+      single-expand
+      show-expand
+    >
       <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>All Medications</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="searchString"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" dark class="mb-2" @click.stop="showAddDialog = true">Add Medication</v-btn>
-          <AddMedicationDialog v-model="showAddDialog"/>
+        <v-card class="elevation-0">
+          <v-card-title>
+            All Medications
+            <v-spacer></v-spacer>
+            <v-btn color="primary" dark class="mb-2" @click.stop="openActionDialog('add')">Add Medication</v-btn>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="searchString"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-text>
+        </v-card>
+          <MedicationActionDialog
+            :action="medicationAction"
+            :initial-medication="editedMedication"
+            v-model="showActionDialog"
+          />
           <ConfimationDialog v-model="showDeleteDialog" @confirm="onDeleteMedication">
             Are you sure you want to delete medication {{ medicationToDelete.name }}?
           </ConfimationDialog>
-        </v-toolbar>
       </template>
       <template v-slot:item.action="{ item }">
+        <v-icon small class="mr-2" @click.stop="openActionDialog('edit', item)">edit</v-icon>
         <v-icon small @click.stop="openDeleteDialog(item)">delete</v-icon>
+      </template>
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">{{ item.description || '-' }}</td>
       </template>
     </v-data-table>
   </v-card>
 </template>
 
 <script>
-  import AddMedicationDialog from '@/components/AddMedicationDialog'
+  import MedicationActionDialog from '@/components/MedicationActionDialog'
   import ConfimationDialog from '@/components/ConfimationDialog'
   import { mapActions, mapGetters } from 'vuex'
   import helpers from '@/mixins/helpers'
 
   export default {
     name: 'Medications',
-    components: { ConfimationDialog, AddMedicationDialog },
+    components: { ConfimationDialog, MedicationActionDialog },
     mixins: [helpers],
     data: () => ({
-      searchString: null,
-      showAddDialog: false,
-      showDeleteDialog: false,
+      editedMedication: {},
+      medicationAction: 'add',
       medicationToDelete: {},
+      searchString: null,
+      showActionDialog: false,
+      showDeleteDialog: false,
       headers: [
         { text: 'Name', align: 'left', value: 'name', },
         { text: 'Category', align: 'left', sortable: false, value: 'category.name' },
         { text: 'Expiration Date', align: 'left', value: 'expiration_date' },
         { text: 'Actions', value: 'action', sortable: false }
-      ],
-      newMedication: {
-        name: '',
-        category: '',
-        production_date: null,
-        expiration_date: null
-      }
+      ]
     }),
     computed: {
       ...mapGetters(['allMedications'])
@@ -63,7 +78,12 @@
       async onDeleteMedication (medication = this.medicationToDelete) {
         await this.deleteMedication({ id: medication.id, categoryId: medication.category.id })
       },
-      openDeleteDialog(medication) {
+      openActionDialog (action, medication = {}) {
+        this.showActionDialog = true
+        this.medicationAction = action
+        this.editedMedication = medication
+      },
+      openDeleteDialog (medication) {
         this.showDeleteDialog = true
         this.medicationToDelete = medication
       }

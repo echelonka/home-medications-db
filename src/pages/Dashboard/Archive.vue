d3495a3284984f4cb7f1901f982b5d55175c23b5
marinaskripnik97@gmail.com <marinaskripnik97@gmail.com>
2019-11-20 00:10:18 +0200

    added archive functionality

--- src/pages/Dashboard/Archive.vue
+++ src/pages/Dashboard/Archive.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-data-table
       :headers="headers"
-      :items="medicationsByCategory(categoryName)"
+      :items="archivedMedications"
       :search="searchString"
       disable-pagination
       hide-default-footer
@@ -11,7 +11,10 @@
     >
       <template v-slot:top>
         <v-card class="elevation-0">
-          <v-card-title>{{ categoryName | capitalize }}</v-card-title>
+          <v-card-title>
+            Archive
+            <v-spacer></v-spacer>
+          </v-card-title>
           <v-card-text>
             <v-text-field
               v-model="searchString"
@@ -23,7 +26,7 @@
           </v-card-text>
         </v-card>
         <ConfirmationDialog v-model="showDeleteDialog" headline="Delete Medication" @confirm="onDeleteMedication">
-          Are you sure you want to delete medication {{ medicationToDelete.name }}?
+          Are you sure you want to delete medication {{ medicationToDelete.name }} from archive?
         </ConfirmationDialog>
       </template>
       <template v-slot:item.action="{ item }">
@@ -38,32 +41,30 @@
 
 <script>
   import { mapActions, mapGetters } from 'vuex'
-  import helpers from '@/mixins/helpers'
 
   export default {
-    name: 'Category',
-    mixins: [helpers],
+    name: 'Archive',
     data: () => ({
-      searchString: null,
-      showDeleteDialog: false,
-      medicationToDelete: {},
       headers: [
         { text: 'Name', align: 'left', value: 'name', },
         { text: 'Category', align: 'left', sortable: false, value: 'category.name', width: 250 },
-        { text: 'Expiration Date', align: 'left', value: 'expiration_date', width: 150 },
         { text: 'Actions', value: 'action', sortable: false, width: 50 }
       ],
+      medicationToDelete: {},
+      searchString: null,
+      showDeleteDialog: false
     }),
-    computed: {
-      ...mapGetters(['medicationsByCategory']),
-      categoryName () {
-        return this.$route.params.category
-      }
+    beforeMount () {
+      this.bindArchive()
     },
+    beforeDestroy () {
+      this.unbindArchive()
+    },
+    computed: mapGetters(['archivedMedications']),
     methods: {
-      ...mapActions(['deleteMedication']),
-      async onDeleteMedication (medication = this.medicationToDelete) {
-        await this.deleteMedication({ id: medication.id, categoryId: medication.category.id })
+      ...mapActions(['deleteFromArchive', 'bindArchive', 'unbindArchive']),
+      async onDeleteMedication () {
+        this.deleteFromArchive(this.medicationToDelete.id)
       },
       openDeleteDialog (medication) {
         this.showDeleteDialog = true
